162c6c5080613d21ef0c0cfd33c71a55ba92d22a
Javier <javucho1000@yahoo.com.mx>
2014-09-09 19:58:07 +0900

    Adding do while to work with mutliple arguments

--- realpath.c
+++ realpath.c
@@ -1,5 +1,28 @@
-/// opt / local / include
-
+/*-
+ * Copyright (c) 2014, Javier
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <sys/syslimits.h>
 
@@ -52,16 +75,20 @@ main(int argc, char **argv)
 	
 	argv += optind;
 	path = *argv != NULL ? *argv++ : ".";
-	if ((rp = realpath(path, resolved_name)) != NULL){
-		printf("%s\n", rp);
+	do{
+		if ((rp = realpath(path, resolved_name)) != NULL){
+			printf("%s\n", rp);
 
-	} else {
-	//If resolved_name was non-NULL, it will contains the pathname which caused the problem.
-		if (!qflag)
-			warn("%s", resolved_name);
-		exit_val =1;
-	}
-
+		} else {
+			if (!qflag)
+				/* If resolved_name was non-NULL, it will
+				 * contains the pathname which caused the
+				 * problem. realpath(3)
+				 */
+				warn("%s", resolved_name);
+			exit_val =1;
+		}
+	} while ((path = *argv++) != NULL);
 
 	exit(exit_val);
 }
