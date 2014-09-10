a20de97644879cf77fc20f458efa39ce2a3071ad
Javier <javucho1000@yahoo.com.mx>
2014-09-11 01:31:40 +0900

    Some work on tests

--- test.sh
+++ test.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+set -o errexit
+set -o nounset
+
+readonly PROGRAM_PATH="./realpath"
+if [ ! -e "$PROGRAM_PATH" ]; then
+	echo "'$PROGRAM_PATH' doesn't exist"
+	exit 1;
+fi
+
+function test_no_option() {
+	readonly RESULT="$($PROGRAM_PATH)"
+	readonly PWD="$(pwd)"
+	if [ "$PWD" == "$RESULT" ];then
+		echo "OK"
+	fi
+	echo ">$RESULT<"
+	echo ">$PWD<"
+}
+
+test_no_option
 
 #no option  = pwd
 
